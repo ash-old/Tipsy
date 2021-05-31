c4dda82b3ce368ee6585f2e02c1ab22245618194
ash-old <ashley.oldham@gmail.com>
2021-05-31 15:51:51 +0100

    added IBActions and IBOulets to new Results Controller

--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var settingsLabel: UILabel!
+  
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
+  @IBAction func recalculatePressed(_ sender: UIButton) {
+  }
+  
     /*
     // MARK: - Navigation
 
