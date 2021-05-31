132a95f7ced4250de905f8716d52f27db5c92cbc
ash-old <ashley.oldham@gmail.com>
2021-05-31 15:23:42 +0100

    IBAction and Outlets linked on main calculator View Controller

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Ash Oldham on 25/05/2021.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+  @IBOutlet weak var billTextField: UITextField!
+  @IBOutlet weak var zeroPctButton: UIButton!
+  @IBOutlet weak var tenPctButton: UIButton!
+  @IBOutlet weak var twentyPctButton: UIButton!
+  @IBOutlet weak var splitNumberLabel: UILabel!
+  
+
+  @IBAction func tipChanged(_ sender: UIButton) {
+    
+  }
+  
+  @IBAction func stepperValueChanged(_ sender: UIStepper) {
+  }
+  
+  
+  @IBAction func calculatePressed(_ sender: UIButton) {
+  }
+}
+
